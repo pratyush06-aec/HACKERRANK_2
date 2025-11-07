e60d498fcdca63471100fb3b87dd7ef5903395d3
pratyush06-aec <pratyush00710@gmail.com>
2025-11-08 00:21:53 +0530

    update_6

---
+++ problem_23.py
@@ -0,0 +1,15 @@
+n, m= map(int, input().split())
+arr= input().split()
+a= set(input().split())
+b= set(input().split())
+
+happiness= 0
+
+for i in arr:
+    if i in a:
+        happiness+= 1
+
+    elif i in b:
+        happiness-= 1
+
+print(happiness)
