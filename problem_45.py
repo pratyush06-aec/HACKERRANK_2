5e11ecec7136c458850b791db564d8dba62eb9f5
pratyush06-aec <pratyush00710@gmail.com>
2025-12-07 23:57:12 +0530

    update_3

---
+++ problem_45.py
@@ -0,0 +1,22 @@
+a= int(input())
+line_1= set(map(input().split()))
+
+n= int(input())
+
+for _ in range(n):
+    operation= input().split()
+    line_2= set(map(int, input().split()))
+
+    if(operation== "intersection_update"):
+        line_1.intersection_update(line_2)
+
+    elif(operation== "update"):
+        line_1.update(line_2)
+
+    elif(operation=="symmetric_difference_update"):
+        line_1.symmetric_difference_update(line_2)
+
+    elif(operation== "difference_update"):
+        line_1.difference_update(line_2)
+
+print(sum(line_2))
