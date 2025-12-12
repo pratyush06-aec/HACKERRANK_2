8ea9e1fbfa648013e1d9cd650214aafea854b58d
pratyush06-aec <pratyush00710@gmail.com>
2025-12-12 17:10:07 +0530

    update_2

--- problem_53.py
+++ problem_53.py
@@ -1,23 +1,9 @@
-# x= int(input())
-# nums= list(map(int, input().split()))
-
-# all_positives= all(i>0 for i in nums)
-
-# has_palindrome= any(str(i)== str(i)[::-1] for i in nums)
-
-
-# print(all_positives and has_palindrome)
-
-
-
 x= int(input())
 nums= list(map(int, input().split()))
 
-for i in nums:
-    all_positives= all(i>0)
+all_positives= all(i>0 for i in nums)
 
-for i in nums:
-    has_palindrome= any(str(i)==str(i)[::-1])
+has_palindrome= any(str(i)== str(i)[::-1] for i in nums)
 
 
 print(all_positives and has_palindrome)
